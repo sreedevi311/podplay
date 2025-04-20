fad024d7800143f08a39e966b2378720ac9bb37d
sreedevi311 <165983022+sreedevi311@users.noreply.github.com>
2025-04-20 07:43:40 +0530

    Update categories.js

--- categories.js
+++ categories.js
@@ -1,54 +1,36 @@
-import {categories} from "./data.js";
+import { categories } from "./data.js";
 
-export function generateCategoriesPage(){
-let fullHTML='';
-  let i=1;
-  categories.forEach( (categorie) => {
-      fullHTML+=
-      `
-        <div class="card color-${i} with-image js-card" data-id="${categorie.id}" data-name="${categorie.name}">
-              <img src="${categorie.image}" alt="Music Category">
-              <span>${categorie.name}</span>
-        </div>
-        `;
-        i++;
+export function generateCategoriesPage() {
+  let fullHTML = '';
+  let i = 1;
+  categories.forEach((categorie) => {
+    fullHTML += `
+      <div class="card color-${i} with-image js-card" data-i="${i}" data-name="${categorie.name}">
+        <img src="${categorie.image}" alt="Music Category">
+        <span>${categorie.name}</span>
+      </div>
+    `;
+    i++;
   });
-      
-  document.querySelector('.js-grid').innerHTML=`${fullHTML}`;
+
+  document.querySelector('.js-grid').innerHTML = fullHTML;
 }
+
 generateCategoriesPage();
-/*
-function categoryClicks(){
-    document.querySelectorAll('.js-card').forEach( (div)=>{
-        div.addEventListener('click',()=>{
-            const {id,name} =div.dataset;
-            generateCategoryPage(id,name);
-        });
-    });
-}
-categoryClicks();
 
-function generateCategoryPage(id,name){
-  let fullHTML='';
-  let i=1;
-  let variable=category.name;
-  variable.forEach( (categorie) => {
-      fullHTML+=
-      `
-        <div class="header">${variable}</div>
-        <div style="background: linear-gradient(color-${i},black); height: 250px;">
-          <div class="section-title">Best episodes of the week</div>
-          <div class="podcast-list">
-            <div class="podcast-item">
-              <img src="${categorie.image}" alt="Podcast 1" />
-              <div class="podcast-title">${categorie.name}</div>
-            </div>
-          </div>
-        </div>
-        `;
-        i++;
+export function categoryClicks() {
+  document.querySelectorAll('.js-card').forEach((div) => {
+    div.addEventListener('click', () => {
+      const { name,i } = div.dataset;
+      const colorClass = `color-${i}`;
+      // Save to sessionStorage
+      sessionStorage.setItem("selectedCategory", name);
+      sessionStorage.setItem("selectedColorClass", colorClass);
+
+      // Navigate to the category page
+      window.location.href = "category.html";
+    });
   });
-      
-  document.querySelector('.js-body').innerHTML=`${fullHTML}`;
 }
-  */
+
+categoryClicks();
